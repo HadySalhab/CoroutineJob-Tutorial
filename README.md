2397d6571f34704adb8e9f86f6f7985427f16f18
Hady Salhab <hadysalhab1@gmail.com>
2020-02-23 17:26:10 +1100

    Complete code

--- README.md
+++ README.md
@@ -1,5 +1,58 @@
 # CoroutineJob-Tutorial
 
-This repository contains code for the following tutorial on Youtube: 
+This repository contains code for the following tutorial on Youtube:
 [Kotlin Coroutine Jobs (Beginner Example)](https://www.youtube.com/watch?v=UsHTxOILP5g)
 
+```kotlin
+    private lateinit var  job: CompletableJob //allows us to invoke complete() on the job to terminate/complete the job without relying on coroutines to do so (More Control)
+```
+
+We can get a **callback** when the job is completed or cancelled using the below method:
+
+```kotlin
+  job.invokeOnCompletion {
+      //handler exception will pass a message which might be null or empty
+        it?.message.let {
+                var msg= it
+                if(msg.isNullOrEmpty()){
+                    msg = "Unknown cancellation error."
+                }
+                println("${job} was cancelled. Reason: $msg")
+            }
+        }
+```
+
+```kotlin
+  CoroutineScope(IO + job).launch {
+//this will launch a coroutine that belongs to the IO dispatcher attached to a job
+//so we can monitor its state (Complete, activte, cancelled,cancelling...)
+            }
+```
+
+```kotlin
+  CoroutineScope(IO).launch {
+//this will launch a coroutine that belongs to the IO dispatcher)
+            }
+```
+
+The difference in the above is the following:
+
+```kotlin
+val coroutineScope = CoroutineScope(IO)
+coroutineScope.cancel()// will cancel all coroutines that belong to the IO dispatcher
+
+val coroutineScope = CoroutineScope(IO+job)
+job.cancel()//will cancel only that job, all coroutines attached to that job
+```
+
+We can get the state of coroutines attached to a job using the following method:
+
+1. job.isActive //true if job is still active
+1. job.isCompleted //true if job is completed
+1. job.isCanceled //true if job is canceled
+
+```kotlin
+job.cancel(CancellationException("Any Message")) //this exception will be caught by  invokeOnCompletion callback
+```
+
+When a job is canceled it cannot be used again, we have to reinitialize a new job to launch more coroutines..
--- app/src/main/java/com/android/myapplication/coroutinejob_tutorial/MainActivity.kt
+++ app/src/main/java/com/android/myapplication/coroutinejob_tutorial/MainActivity.kt
@@ -2,11 +2,104 @@ package com.android.myapplication.coroutinejob_tutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ProgressBar
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
 
 class MainActivity : AppCompatActivity() {
+
+
+    private val TAG: String = "AppDebug"
+
+    private val PROGRESS_MAX = 100
+    private val PROGRESS_START = 0
+    private val JOB_TIME = 4000 // ms
+    private lateinit var job: CompletableJob
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        job_button.setOnClickListener {
+            if(!::job.isInitialized){
+                initjob()
+            }
+            job_progress_bar.startJobOrCancel(job)
+        }
+    }
+
+    fun resetjob(){
+        if(job.isActive || job.isCompleted){
+            //this cancellation exception will be caught by invokeOnCompletion callback
+            job.cancel(CancellationException("Resetting job"))
+        }
+        initjob() //we should reinitialize a job after being canceled
+    }
+
+    //job configuration
+    fun initjob(){
+        job_button.setText("Start Job #1")
+        updateJobCompleteTextView("")
+        job = Job()
+        //callback when the job is cancelled or completed
+        job.invokeOnCompletion {
+            it?.message.let{
+                var msg = it
+                if(msg.isNullOrBlank()){
+                    msg = "Unknown cancellation error."
+                }
+                Log.e(TAG, "${job} was cancelled. Reason: ${msg}")
+                showToast(msg)
+            }
+        }
+        job_progress_bar.max = PROGRESS_MAX
+        job_progress_bar.progress = PROGRESS_START
+    }
+
+
+    //Progress bar in android can be updated on the background thread.
+    fun ProgressBar.startJobOrCancel(job: Job){
+        if(this.progress > 0){
+            Log.d(TAG, "${job} is already active. Cancelling...")
+            resetjob() //if the job is active , cancelled.
+        }
+        else{
+            //if the job is not active.. run it
+            job_button.setText("Cancel Job #1")
+            //running a coroutine attached to that job on the io dispatcher
+            CoroutineScope(IO + job).launch{
+                Log.d(TAG, "coroutine ${this} is activated with job ${job}.")
+
+                for(i in PROGRESS_START..PROGRESS_MAX){
+                    delay((JOB_TIME / PROGRESS_MAX).toLong())
+                    this@startJobOrCancel.progress = i
+                }
+                withContext(Main) {
+                    updateJobCompleteTextView("Job is complete!")
+                }
+            }
+        }
+    }
+
+     private  fun updateJobCompleteTextView(text: String){
+
+            job_complete_text.setText(text)
+
+    }
+
+    private fun showToast(text: String){
+        GlobalScope.launch (Main){
+            Toast.makeText(this@MainActivity, text, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
     }
 }
